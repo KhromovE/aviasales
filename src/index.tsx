c4ac4e84b990e93627d0d28b58b628e393a91ef2
Egor <khromove@gmail.com>
2020-05-06 12:29:09 +0700

    remove router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,6 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
 
 import { App } from './app'
 
-ReactDOM.render(
-  <BrowserRouter basename={process.env.BASE_URL}>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
-)
+ReactDOM.render(<App />, document.getElementById('root'))
